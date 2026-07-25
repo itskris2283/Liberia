813622ad0ae03ac96f08d9a09aedec62c6ef2d29
itskris2283 <krisprog2283@gmail.com>
2026-07-25 17:53:04 +0530

    Add dynamic time of day greeting (morning, afternoon, evening, night)

--- app.js
+++ app.js
@@ -842,6 +842,23 @@ class BookShelfApp {
         }
 
         this.updateQuote();
+        this.updateGreeting();
+    }
+
+    updateGreeting() {
+        if (!this.el.greeting_title) return;
+        const hour = new Date().getHours();
+        let greeting = "Good evening";
+        if (hour >= 5 && hour < 12) {
+            greeting = "Good morning";
+        } else if (hour >= 12 && hour < 17) {
+            greeting = "Good afternoon";
+        } else if (hour >= 17 && hour < 22) {
+            greeting = "Good evening";
+        } else {
+            greeting = "Good night";
+        }
+        this.el.greeting_title.textContent = `${greeting}, reader. ✨`;
     }
 
     updateQuote() {
